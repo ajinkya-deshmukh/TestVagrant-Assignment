2447829e0aabcc86aa3d6f391df114dd812d46e2
Ajinkya Deshmukh <ajinkyadeshmukh@AjinkyaD-InteliMac.local>
2020-12-30 15:31:20 +0530

    Added response code check to get weather api

--- common/services/fetch_temperature.py
+++ common/services/fetch_temperature.py
@@ -12,7 +12,11 @@ class TemperatureService():
             "appid": config.API_KEY
         }
         response = requests.get(con.open_weather_url + "?q=" + config.city_name, params=parameters)
-        temp_data = response.json()['main']['temp']
-        temp_data = temp_data - 273.15  # Convert temp Kelvin to Degree Celsius
-        temp_data = str(round(temp_data, 2))
-        return temp_data
+        if response.status_code == 200:
+            print("Get Weather API status code is 200")
+            temp_data = response.json()['main']['temp']
+            temp_data = temp_data - 273.15  # Convert temp Kelvin to Degree Celsius
+            temp_data = str(round(temp_data, 2))
+            return temp_data
+        else:
+            print("Get Weather API has failed")
