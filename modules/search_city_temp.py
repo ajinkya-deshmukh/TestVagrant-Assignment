4e1c91e8733831985a60e9e6b609b901a29cefcf
Ajinkya Deshmukh <ajinkyadeshmukh@AjinkyaD-InteliMac.local>
2020-12-30 11:16:29 +0530

    Added config file to read from env. Created service layer to fetch temprature from openweather api.

--- modules/search_city_temp.py
+++ modules/search_city_temp.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from common.object_repository.ndtv_home import NDTVHome
 from common.util.constants import Constants
 from common.util.common_methods import CommonMethods
+from common.services.fetch_temperature import TemperatureService
+from common.object_repository.weather_map import WeatherMap
 
 @pytest.mark.smokeTestDebug
 @pytest.fixture
@@ -12,9 +17,12 @@ def test_pyfixtureInit():
     global URL
     driver = webdriver.Chrome()
     con = Constants()
-    URL = con.URL
-    driver.get(URL)
+    url = con.NDTV_url
+    #change this driver.get later
+    driver.get("https://social.ndtv.com/static/Weather/report/")
     driver.maximize_window()
+  #  alert = Alert(driver)
+  #  alert.accept()
     yield
     print("Initiating Tear Down")
     #driver.quit()
@@ -23,10 +31,26 @@ def test_verify_weather_map(test_pyfixtureInit):
     print("Search City Temperature Test Started")
 
     common_methods = CommonMethods()
-    common_methods.test_navigate_to_weather_map()
+    temp_service = TemperatureService()
+    ndtv_obj_repo = NDTVHome()
+    weather_map = WeatherMap()
+
+    # 3 lines to be uncommented for test current test purpose
+
+   # WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, ndtv_obj_repo.btn_accept_alert)))
+
+   # driver.find_element_by_xpath(ndtv_obj_repo.btn_accept_alert).click()
+   # common_methods.test_navigate_to_weather_map(driver)
+
+    # This is a bypass and should be removed
+
+   # driver.get("https://social.ndtv.com/static/Weather/report/")
 
     # Verify elements on the weather map
 
+    temp_service.function_get_city_temp()
+
+    txt_search = driver.find_element_by_xpath(weather_map.img_weather_logo)
 
 
 
